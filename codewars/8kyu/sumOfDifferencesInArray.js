2d581ba651b43d1428da7a3ef1bd3e0bc7cd5431
LuisDFlores <floresluis242@gmail.com>
2022-05-22 23:06:12 -0400

    first try

---
+++ codewars/8kyu/sumOfDifferencesInArray.js
@@ -0,0 +1,12 @@
+function sumOfDifferences(arr) {
+    if (arr.length == 0 || arr.length == 1) {
+        return 0
+    }
+    arr.sort((a, b) => b - a);
+    let sum = 0
+    for (let i = 0; i < arr.length - 1; i++) {
+        sum = sum + (arr[i] - arr[i + 1])
+    }
+
+    return sum
+}
