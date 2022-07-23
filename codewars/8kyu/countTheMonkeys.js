61ed7826c6e89d44b3c7f52897a60d9d49671099
LuisDFlores <floresluis242@gmail.com>
2022-07-23 00:04:47 -0400

    solution

--- codewars/8kyu/countTheMonkeys.js
+++ codewars/8kyu/countTheMonkeys.js
@@ -6,3 +6,12 @@
 
 // 10 -- > [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // 1 -- > [1]
+
+
+function monkeyCount(n) {
+    let count = [];
+    for (let i = 1; i <= n; i++) {
+        count.push(i);
+    }
+    return count
+}
